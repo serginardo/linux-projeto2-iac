8c8e42035d11f68fabd05209d6091086719ac553
serginardo <serginardo@gmail.com>
2023-03-05 15:33:12 -0300

    Apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get install apache unzip -y 
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+
